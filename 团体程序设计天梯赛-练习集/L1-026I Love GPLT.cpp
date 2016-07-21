dbb1557eec03ae339610960e54c29a3819629d0c
yizhixiaobai <1042897963@qq.com>
2016-07-21 11:18:30 +0800

    https://www.patest.cn/contests/gplt/L1-026
    水

---
+++ 团体程序设计天梯赛-练习集/L1-026I Love GPLT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "I" << endl
+		<< " " << endl
+		<< "L" << endl
+		<< "o" << endl
+		<< "v" << endl
+		<< "e" << endl
+		<< " " << endl
+		<< "G" << endl
+		<< "P" << endl
+		<< "L" << endl
+		<< "T" << endl;
+	return 0;
+}
